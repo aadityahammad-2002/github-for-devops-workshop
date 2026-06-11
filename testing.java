e61138f8402b56070d371ca97c4fed1d1e05ea6c
Aaditya Hammad <aadityahammad@gmail.com>
2026-06-11 19:46:21 +0530

    new line added

--- testing.java
+++ testing.java
@@ -1,6 +1,9 @@
 public class testing {
     public static void main(String[] args) {
         System.out.println("Hello Dosto, Kya Haal chall");
+        int a = 10;
+        int b = 20;
+        System.out.println("value of a: " + a + "value of b: " + b);
     }
 
 }
